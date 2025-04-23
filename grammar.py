fe6a4ffec659a4514ce8e47662f2efe24ec5ca44
FirebirdCS <alvaritotutos@gmail.com>
2025-04-23 15:19:27 -0600

    Buffer añadido

--- grammar.py
+++ grammar.py
@@ -1,25 +1,18 @@
-import sys
 from Lexic_ import lexer
+from Buffer import Buffer
 
 def main():
-    if len(sys.argv) != 2:
-        print("Uso: python lexer_only.py MiLenguaje.compi")
-        sys.exit(1)
+    buf = Buffer()
 
-    archivo = sys.argv[1]
-    try:
-        with open(archivo, 'r', encoding='utf-8') as f:
-            data = f.read()
-    except FileNotFoundError:
-        print(f"No se encontró el archivo: {archivo}")
-        sys.exit(1)
+    print("Tokens encontrados:\n")
+    # Recorremos cada "chunk" de hasta 10 líneas
+    for chunk in buf.load_buffer():
+        # Alimentamos el lexer con el bloque de texto
+        lexer.input(chunk)
 
-    # Alimentamos el contenido al lexer
-    lexer.input(data)
-
-    print(f"Tokens encontrados en '{archivo}':\n")
-    for tok in lexer:
-        print(f"{tok.type:20}  {tok.value!r}   (línea {tok.lineno})")
+        # Recorremos y mostramos todos los tokens de este bloque
+        for tok in lexer:
+            print(f"{tok.type:20} {tok.value!r}   (línea {tok.lineno})")
 
 if __name__ == "__main__":
     main()
